edf52f557d01b138e6c40c0d8cba277c0fedd2de
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-06-07 17:59:34 +0300

    Update main link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ const Home = () => {
                 <p className='text-gray-500 font-medium max-w-2xl text-center leading-8 mb-10'>Платформа, створена для ефективного представлення та просування ваших професійних навичок у цифровому
                     світі.</p>
                 <div>
-                    <Link className='mx-4 font-semibold px-6 py-3 text-white bg-sky-500 rounded-md' href='/'>Почати</Link>
+                    <Link className='mx-4 font-semibold px-6 py-3 text-white bg-sky-500 rounded-md' href='/login'>Почати</Link>
                     <Link className='mx-4 font-semibold hover:border-b hover:border-black transition ease-out' href='/about-platform'>Дізнатися більше &#x2794;</Link>
                 </div>
             </main>
