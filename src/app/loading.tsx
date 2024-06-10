abed5e74a0fdf506d85ab3012d01b0ebcf1603c7
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-06-10 23:58:26 +0300

    Add loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import Loader from "@/components/ui/Loader/Loader"
+
+const Loading = () => {
+  return (
+    <div className='flex-1 flex flex-col items-center justify-center'>
+      <Loader />
+    </div>
+  )
+}
+
+export default Loading
